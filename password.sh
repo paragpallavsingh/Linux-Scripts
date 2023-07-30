40fac467466c994719955721d606a21e2d13a0dd
Ubuntu <ubuntu@ip-172-31-42-241.eu-north-1.compute.internal>
2023-07-30 13:46:39 +0000

    script 1

---
+++ password.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+## this script will add a new user and add password
+read -p "Enter username\n" username
+sudo useradd $username
+read -s -p "Please enter your password: " password
+sudo passwd $username
+printf "password updated succesfully\n see in list\n"
+cat /etc/passwd | grep $username
